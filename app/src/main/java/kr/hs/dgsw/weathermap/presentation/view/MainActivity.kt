8513ff54be7b66ac0698abd89790acd37c0f3df8
사승은 <gsasy0113@gmail.com>
2022-04-06 13:34:37 +0900

    Setting: Init Project

---
+++ app/src/main/java/kr/hs/dgsw/weathermap/presentation/view/MainActivity.kt
@@ -0,0 +1,10 @@
+package kr.hs.dgsw.weathermap.presentation.view
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
